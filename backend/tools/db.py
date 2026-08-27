21222a6b2bf25e8d702d2a2ed0a724f809be345e
Ahmad Imran <ahmadimran67208@gmail.com>
2026-08-27 21:17:33 +0500

    add register vector function in the db config

--- backend/tools/db.py
+++ backend/tools/db.py
@@ -1,11 +1,15 @@
 from psycopg_pool import ConnectionPool
 from psycopg.rows import dict_row
 from dotenv import load_dotenv
+from pgvector.psycopg import register_vector
 import os
 
 load_dotenv()
 
 POSTGRES_URI = os.getenv("POSTGRES_URI")
+
+def configure_connection(conn):
+    register_vector(conn)
 
 pool = ConnectionPool(
   conninfo=POSTGRES_URI, 
@@ -16,6 +20,7 @@ pool = ConnectionPool(
     "row_factory": dict_row
   },
   check=ConnectionPool.check_connection,
+  configure=configure_connection,
 )
 
 
