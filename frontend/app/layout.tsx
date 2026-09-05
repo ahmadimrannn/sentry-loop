e19fbcadc1cc57461d40999081dc1f0211f108b1
Ahmad Imran <ahmadimran67208@gmail.com>
2026-09-05 14:06:32 +0500

    Implement vercel web analytics for tracking the traffic

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from '@vercel/analytics/next';
 import { Geist, Inter_Tight } from "next/font/google";
 import "./globals.css";
 
@@ -28,6 +29,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
             suppressHydrationWarning
             className="min-h-full flex flex-col font-sans">
                 {children}
+                <Analytics />
         </body>
     </html>
   );
