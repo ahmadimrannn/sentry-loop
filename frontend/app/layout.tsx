a927152458fbb06f7f453e3cf872d0b0f25d8fab
Ahmad Imran <ahmadimran67208@gmail.com>
2026-09-09 18:29:40 +0500

    Add google analytics in my website

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Analytics } from '@vercel/analytics/next';
 import { Geist, Inter_Tight } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-sans",
@@ -25,6 +26,21 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
     suppressHydrationWarning
     className={`${geistSans.variable} ${interTight.variable} font-sans h-full antialiased`}
     >
+        <head>
+            <Script
+                src="https://www.googletagmanager.com/gtag/js?id=G-V3D3NDLZZB"
+                strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'G-V3D3NDLZZB');
+                `}
+            </Script>
+        </head>
         <body 
             suppressHydrationWarning
             className="min-h-full flex flex-col font-sans">
